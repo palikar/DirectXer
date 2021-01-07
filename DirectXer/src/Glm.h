fbf09ca42cce4cc42e8aadabcbce0da5aff1ef4c
Stanislav Arnaudov <stanislav.arn@gmail.com>
2021-01-07 23:06:24 +0200

    Start doing some good work! I like it know! I love putting code on the screen!

---
+++ DirectXer/src/Glm.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define GLM_FORCE_SWIZZLE
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <glm/ext/quaternion_transform.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtx/vec_swizzle.hpp>
