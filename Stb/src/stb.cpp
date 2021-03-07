a200690aa8296595515787a8c7ee386683a36447
Stanislav Arnaudov <stanislav.arn@gmail.com>
2021-03-07 23:18:09 +0200

    Add stb rect pack

--- Stb/src/stb.cpp
+++ Stb/src/stb.cpp
@@ -2,7 +2,9 @@
 #define STB_SPRINTF_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_RECT_PACK_IMPLEMENTATION
 #include <stb_image.h>
 #include <stb_image_write.h>
 #include <stb_image_resize.h>
 #include <stb_sprintf.h>
+#include <stb_rect_pack.h>
